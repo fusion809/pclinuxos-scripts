b4493cc80292d454349b3cf000caa58b5d4c4912
fusion809 <brentonhorne77@gmail.com>
2018-06-09 23:22:41 +1000

    Rm redundant scripts, moving what remains into common-scripts

--- Shell/variables/02-github.sh
+++
@@ -1,6 +0,0 @@
-# GHUB variables
-export GHUB=$HOME/GitHub
-export GHUBM=$GHUB/mine
-export GHUBO=$GHUB/others
-export FGI=$GHUBM/fusion809.github.io
-export DBS=$GHUBM/scripts/pclinuxos-scripts
--- Shell/variables/03-path.sh
+++
@@ -1,1 +0,0 @@
-export JAVA_HOME=/usr/lib/jvm/oracle-jdk-bin-1.8/bin
